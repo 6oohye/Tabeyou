d2bbe86a2ae1219fb86ca090d9b4bd911797226c
hyejin <6oohye@naver.com>
2024-05-11 19:01:31 +0900

    [create] Network folder 生成.

---
+++ Tabeyou/Tabeyou/Network/NetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkService.swift
+//  Tabeyou
+//
+//  Created by 6혜진 on 5/11/24.
+//
+
+import Foundation
+//class NetworkService{
+//    static let shard : NetworkService = NetworkService()
+//
+//}
