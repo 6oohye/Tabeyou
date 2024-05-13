e6f4ebd2d3bd779bf68689574e2ba6e87b602c3c
hyejin <6oohye@naver.com>
2024-05-13 19:05:47 +0900

    [create] ResultTableView レイアウト完成

---
+++ Tabeyou/Tabeyou/Feature/Result/Cell/ResultTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ResultTableViewCell.swift
+//  Tabeyou
+//
+//  Created by 6혜진 on 5/13/24.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    
+    static let identifire: String = "ResultTableViewCell"
+    static let height : CGFloat = 100
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var restaurantImageView: UIImageView!
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var stationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.containerView.layer.borderColor = AppColors.UIKit.gray0.cgColor
+        self.containerView.layer.borderWidth = 1
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Tabeyou/Tabeyou/Feature/Result/ResultListViewController.swift
+++ Tabeyou/Tabeyou/Feature/Result/ResultListViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ResultListViewController: UIViewController {
     
+    enum ResultSection: Int, CaseIterable{
+        case restaurant
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationBarCustom()
+        
+        self.setupTableView()
 
+    }
+    
+    //MARK: - ResultTableViewCell呼び出す関数
+    private func setupTableView() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UINib(nibName: ResultTableViewCell.identifire, bundle: nil),
+                                forCellReuseIdentifier: ResultTableViewCell.identifire)
     }
     
 }
@@ -38,4 +54,36 @@ extension ResultListViewController{
     }
     
 }
+
+extension ResultListViewController : UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        ResultSection.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = ResultSection(rawValue: section) else{
+            return 0
+        }
+        
+        switch section{
+        case .restaurant :
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = ResultSection(rawValue: indexPath.section) else {
+            return UITableViewCell()
+        }
+        
+        switch section{
+        case .restaurant :
+            return tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.identifire, for: indexPath)
+        }
+    }
+    
+    
+}
+
+
 
