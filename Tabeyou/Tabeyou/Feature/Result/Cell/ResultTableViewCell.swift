3095d7698491d39ca5861dafecb708d850fa3a03
hyejin <6oohye@naver.com>
2024-05-15 15:18:01 +0900

    [update] ResultListViewCellView レイアウトエラーの解決

--- Tabeyou/Tabeyou/Feature/Result/Cell/ResultTableViewCell.swift
+++ Tabeyou/Tabeyou/Feature/Result/Cell/ResultTableViewCell.swift
@@ -28,6 +28,8 @@ class ResultTableViewCell: UITableViewCell {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     
+    
+    
     func setViewModel(_ viewModel : ResultTableViewCellViewModel){
         restaurantImageView.kf.setImage(with: URL(string: viewModel.imageUrl))
         restaurantNameLabel.text = viewModel.title
@@ -43,12 +45,35 @@ class ResultTableViewCell: UITableViewCell {
         self.containerView.layer.borderColor = AppColors.UIKit.gray0.cgColor
         self.containerView.layer.borderWidth = 1
         
+        // restaurantImageView 제약 조건 설정
+        restaurantImageView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(restaurantImageView)
+        
+        NSLayoutConstraint.activate([
+            // 이미지뷰 크기 고정
+            restaurantImageView.heightAnchor.constraint(equalToConstant: 80),
+            
+            // 이미지뷰를 containerView의 중앙에 정렬
+            restaurantImageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            
+            // containerView의 상단과의 제약 조건 (우선 순위를 낮춤)
+            restaurantImageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10).withPriority(.defaultLow),
+            restaurantImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20).withPriority(.defaultLow)
+            
+        ])
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
+}
+
+private extension NSLayoutConstraint {
+    func withPriority(_ priority: UILayoutPriority) -> NSLayoutConstraint {
+        self.priority = priority
+        return self
+    }
 }
