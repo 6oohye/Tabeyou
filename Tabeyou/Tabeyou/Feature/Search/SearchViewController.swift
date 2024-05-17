786667c2187bcba58c5f62a92ec6c0abffefad7e
hyejin <6oohye@naver.com>
2024-05-18 03:30:18 +0900

    [update] レイアウト·デバッグ作業

--- Tabeyou/Tabeyou/Feature/Search/SearchViewController.swift
+++ Tabeyou/Tabeyou/Feature/Search/SearchViewController.swift
@@ -11,17 +11,25 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         // 콘텐트뷰의 크기를 설정합니다.
         scrollView.contentSize = contentView.frame.size
         
         // 세로 스크롤이 항상 가능하도록 설정합니다.
-              scrollView.alwaysBounceVertical = true
+        scrollView.alwaysBounceVertical = true
         
         // 콘텐트뷰가 화면 하단에 고정되도록 contentInset을 설정합니다.
-                scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: contentView.frame.height, right: 0)
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: contentView.frame.height, right: 0)
+    
+        //SearchBar UI
+        self.searchBar.layer.cornerRadius = 5
+        self.searchBar.searchBarStyle = .minimal
+        
     }
     
 
